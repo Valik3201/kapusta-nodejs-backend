2893c478b3daf774d63a30ef7f64cfb7e43910c3
superfilar <mateusz9650@gmail.com>
2024-05-14 18:51:06 +0200

    database Shemas
    Shema of User and Transactions

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      minlength: 3,
+      maxlenght: 254,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      minlength: 8,
+      maxlenght: 100,
+      required: [true, "Password is required"],
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
+    sid: {
+      type: String,
+      default: null,
+    },
+    balance: {
+      type: String,
+    },
+    transactions: {
+      description: String,
+      category: String,
+      amount: Number,
+      date: String,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
