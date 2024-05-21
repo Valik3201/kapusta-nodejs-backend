5329cc1ad747f0b48c5230339d23f0cb66cfb813
Valik <valik3201@gmail.com>
2024-05-21 21:02:08 +0200

    chore(html): updated static index.html for server

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-const express = require("express");
 const app = require("./app");
-const path = require("path");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
